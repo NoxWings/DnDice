a29406b736b5321a4fd6765514c291f3c75d2607
NoxWings <david@gamevy.com>
2018-08-26 14:16:28 +0200

    Group up some dice notation tokens

--- src/dice_notation/lexer.ts
+++ src/dice_notation/lexer.ts
@@ -1,13 +1,21 @@
 import { createToken, Lexer } from "chevrotain";
 
 const WhiteSpace = createToken({ name: "WhiteSpace", pattern: /\s+/, group: Lexer.SKIPPED });
+
 const LParen = createToken({ name: "LParen", pattern: /\(/ });
 const RParen = createToken({ name: "RParen", pattern: /\)/ });
-const Plus = createToken({ name: "Plus", pattern: /\+/ });
-const Minus = createToken({ name: "Minus", pattern: /\-/ });
-const Multiply = createToken({ name: "Multiply", pattern: /\*/ });
-const Advantage = createToken({ name: "Advantage", pattern: /H\d*/ });
-const Disadvantage = createToken({ name: "Disadvantage", pattern: /L\d*/ });
+
+const AdditionOperator = createToken({ name: "AdditionOperator", pattern: Lexer.NA });
+const Plus = createToken({ name: "Plus", pattern: /\+/, categories: AdditionOperator });
+const Minus = createToken({ name: "Minus", pattern: /\-/, categories: AdditionOperator });
+
+const MultiplicationOperator = createToken({ name: "MultiplicationOperator", pattern: Lexer.NA });
+const Multiply = createToken({ name: "Multiply", pattern: /\*/, categories: MultiplicationOperator });
+
+const DiceModifier = createToken({ name: "DiceModifier", pattern: Lexer.NA })
+const Advantage = createToken({ name: "Advantage", pattern: /H\d*/, categories: DiceModifier });
+const Disadvantage = createToken({ name: "Disadvantage", pattern: /L\d*/, categories: DiceModifier });
+
 const Dice = createToken({ name: "Dice", pattern: /d\d+/ });
 const Integer = createToken({ name: "Integer", pattern: /\d+/ });
 
@@ -21,7 +29,10 @@ export const tokens = {
     Advantage,
     Disadvantage,
     Dice,
-    Integer
+    Integer,
+    AdditionOperator,
+    MultiplicationOperator,
+    DiceModifier
 };
 
 export const lexer = new Lexer(Object.values(tokens));
