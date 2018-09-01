8fb15de912f93f29dbd0e06cd6b0262797cb974a
NoxWings <david@gamevy.com>
2018-09-01 14:20:56 +0200

    Update index example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { d } from "./RollDistribution";
+import { Roll } from "./Roll";
 
-const magicMissile = d(4).add(d(1)).multiply(3);
+const magicMissile = new Roll("3d4+3");
+const magicMissile2 = new Roll("3(1d4+1)");
+const learnSpell = new Roll("1d20+1d6+1d4");
 
 console.log(magicMissile);
-
-
-
+console.log(magicMissile2);
+console.log(learnSpell);
