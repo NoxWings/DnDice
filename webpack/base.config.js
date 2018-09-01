920a70e1e3016035caef1b9545c750661dd019d3
NoxWings <david@gamevy.com>
2018-09-02 01:43:42 +0200

    Webpack bundle css

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,16 +1,27 @@
 const FOLDERS = require("./constants").FOLDERS;
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: path.join(FOLDERS.SRC, "index.ts"),
+    entry: path.join(FOLDERS.SRC, "index.tsx"),
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "../" }
+                    },
+                    'css-loader',
+                ],
+                include: /resources\/css/
             }
         ]
     },
@@ -18,6 +29,10 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js']
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
         new HtmlWebpackPlugin({ title: "DnDice" })
     ],
     output: {
