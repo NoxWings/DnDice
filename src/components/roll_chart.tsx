bfdab5f522885c0f9e2b0a1ee217b3fa04ad3138
NoxWings <david@gamevy.com>
2018-09-04 00:52:41 +0200

    Update RollChart to display the distribution data

--- src/components/roll_chart.tsx
+++ src/components/roll_chart.tsx
@@ -1,14 +1,39 @@
 import { h, Component } from "preact";
 import { Roll } from "../models/Roll";
+import { Line } from "preact-chartjs-2";
+import { createDataset } from "../data_helpers/create_dataset";
+import { chartOptions } from "../data_helpers/chart_options";
 
 export interface RollChartProps {
     rolls: Readonly<Roll[]>;
 }
 
-export class RollChart extends Component<RollChartProps, {}> {
-    public render({ rolls }: RollChartProps) {
+export interface RollChartState {
+    datasets: object;
+}
+
+export class RollChart extends Component<RollChartProps, RollChartState> {
+    constructor(props) {
+        super(props);
+
+        this.updateChartData();
+    }
+
+    public componentDidUpdate(prevProps) {
+        if (prevProps !== this.props) {
+            this.updateChartData();
+        }
+    }
+
+    public render() {
         return <div>
             <h1>Roll chart</h1>
+            <Line data={this.state} options={chartOptions} />
         </div>;
+    }
+
+    private updateChartData() {
+        const datasets = this.props.rolls.map(createDataset);
+        this.setState({ datasets });
     }
 }
