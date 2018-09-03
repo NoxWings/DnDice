c638e4a4036729e19e1fdcba5a23213b4ab6204f
NoxWings <david@gamevy.com>
2018-09-03 23:34:59 +0200

    Implement main page logic

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,16 +1,42 @@
 import { h, Component } from "preact";
+import { RollList } from "./roll_list";
+import { RollChart } from "./roll_chart";
 import { Roll } from "../models/Roll";
+import { clone } from "lodash";
 
-export class Page extends Component {
+interface PageState {
+    rolls: Roll[];
+}
+
+export class Page extends Component<{}, PageState> {
+    constructor(props) {
+        super(props);
+
+        this.state = { rolls: [] };
+    }
+
     public render() {
-        const magicMissile = new Roll("3d4+3");
-        const magicMissile2 = new Roll("3(1d4+1)");
-        const learnSpell = new Roll("1d20+1d6+1d4");
-
         return <div>
-            <p>{magicMissile.string}</p>
-            <p>{magicMissile2.string}</p>
-            <p>{learnSpell.string}</p>
+            <RollList
+                rolls={this.state.rolls}
+                onRollAdd={this.addRoll.bind(this)}
+                onRollRemove={this.removeRoll.bind(this)}
+            />
+            <RollChart rolls={this.state.rolls} />
         </div>;
+    }
+
+    public addRoll(roll) {
+        const rolls = clone(this.state.rolls);
+        rolls.push(roll);
+
+        this.setState({ rolls });
+    }
+
+    public removeRoll(i) {
+        const rolls = clone(this.state.rolls);
+        rolls.splice(i, 1);
+
+        this.setState({ rolls });
     }
 }
